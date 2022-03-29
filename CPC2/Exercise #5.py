c12b1e7c921a2aff6b2f1a57867f33044c33e005
jushiiko <jsAblay@mcm.edu.ph>
2022-03-29 14:05:35 +0800

    init commit

--- CPC2/Exercise #5.py
+++ CPC2/Exercise #5.py
@@ -5,7 +5,7 @@ f = open('CPC2\Exercise #5.html', 'w')
 message = """<html>
 <title>My First HTML</title>
 <head></head>
-<body><h1>Welcome to my First HTML Program Using Python!</h1>
+<body><h1>Welcome to My First HTML Program Using Python!</h1>
 <h2>My name is John Sebastian Ablay</h2>
 <p>Thank you!</p></body>
 </html>"""
