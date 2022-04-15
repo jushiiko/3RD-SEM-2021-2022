c17add92036990440072fa228156ace61177ee39
jushiiko <jsAblay@mcm.edu.ph>
2022-04-15 23:02:41 +0800

    init commit

---
+++ CPC2/M3/Mysql.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "1234",
+)
+
+my_cursor = mydb.cursor()
+
+my_cursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase")
+
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
